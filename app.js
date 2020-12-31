10a0f2f63bc9c2a66b7653fb888993c2de8d5b44
Rishi Srivastava <sririshi2612@gmail.com>
2021-01-01 00:07:00 +0530

    intital commit
    added all files.

---
+++ app.js
@@ -0,0 +1,28 @@
+var btnCheck = document.querySelector("#btn-check");
+
+var numberInput = document.querySelector("#number-input");
+
+var outputDiv = document.querySelector("#output");
+var score = 0;
+
+function clickHandler(event) {
+    var inputNumber = numberInput.value;
+
+    if (isNaN(inputNumber)){
+        outputDiv.innerHTML = "Error: Please input number"
+    }else {
+        for(var i = 2; i<inputNumber; i++){
+            if (inputNumber % i === 0){
+                score = score + 1;
+            }
+        }
+
+        if(score === 0){
+            outputDiv.innerHTML = inputNumber + " is a Prime Number";
+        }else{
+            outputDiv.innerHTML = inputNumber + " is not a Prime Number";
+        }
+    }
+}
+
+btnCheck.addEventListener("click", clickHandler);
